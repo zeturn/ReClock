c737a8ada92e2c23d3fa66e9dfc96bc4985820ef
henry <zhr626@outlook.com>
2025-01-12 16:05:43 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useEffect } from "react";
+import Clock from "./Clock"; // 假设 Clock 组件位于同一目录下的 Clock.js 文件
+
+import NoSleep from "nosleep.js";
+
+const App = () => {
+    const noSleep = new NoSleep();
+    let wakeLockEnabled = false;
+
+    useEffect(() => {
+        const toggleEl = document.querySelector("#toggle");
+        const handleToggle = () => {
+            if (!wakeLockEnabled) {
+                noSleep.enable(); // 保持屏幕常亮
+                wakeLockEnabled = true;
+                toggleEl.textContent = "屏幕常亮已开启";
+                toggleEl.classList.add("bg-green-500", "hover:bg-green-600");
+                toggleEl.classList.remove("bg-red-500", "hover:bg-red-600");
+            } else {
+                noSleep.disable(); // 允许屏幕熄灭
+                wakeLockEnabled = false;
+                toggleEl.textContent = "屏幕常亮已关闭";
+                toggleEl.classList.add("bg-red-500", "hover:bg-red-600");
+                toggleEl.classList.remove("bg-green-500", "hover:bg-green-600");
+            }
+        };
+
+        toggleEl.addEventListener("click", handleToggle);
+
+        // 清除事件监听器
+        return () => {
+            toggleEl.removeEventListener("click", handleToggle);
+        };
+    }, []);
+
+    return (
+        <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+            <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r w-full">
+                <Clock/>
+                <button
+                    id="toggle"
+                    className="mt-8 px-6 py-3 text-lg font-bold text-white bg-red-500 rounded-lg shadow-lg hover:bg-red-600 transition duration-300"
+                >
+                    屏幕常亮已关闭
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/Clock.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+
+const Clock = () => {
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+
+    return (
+        <div className="w-11/12 md:w-3/4 lg:w-2/3 p-8 bg-white shadow-xl rounded-lg flex justify-center">
+            <div className="text-left p-4">
+                <h1 className="text-3xl md:text-3xl font-extrabold text-gray-500 mb-6">
+                    当前时间
+                </h1>
+                <h2 className="text-5xl md:text-9xl font-semibold font-mono text-indigo-700">
+                    {time.toLocaleTimeString()}
+                </h2>
+            </div>
+        </div>
+
+
+    );
+};
+
+export default Clock;

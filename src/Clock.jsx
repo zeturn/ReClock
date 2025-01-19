161604edc4747b036eb5725d536053b91e30632e
henry <zhr626@outlook.com>
2025-01-18 18:56:48 -0800

    添加年月日

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -18,16 +18,15 @@ const Clock = () => {
             className="w-11/12 md:w-3/4 lg:w-2/3 p-8 bg-white dark:bg-gray-800 shadow-xl rounded-lg flex justify-center">
             <div className="text-left p-4">
                 <h1 className="text-3xl md:text-3xl font-extrabold text-gray-500 dark:text-gray-300 mb-6">
-                    当前时间
+                    {time.toLocaleDateString()} {/* 显示年月日 */}
                 </h1>
-                <h2 className="text-5xl md:text-9xl font-semibold font-mono text-indigo-700 dark:text-indigo-300">
-                    {time.toLocaleTimeString()}
+                <h2 className="text-6xl md:text-9xl font-semibold font-mono text-indigo-700 dark:text-indigo-300">
+                    {time.toLocaleTimeString()} {/* 显示时间 */}
                 </h2>
             </div>
         </div>
-
-
     );
 };
 
 export default Clock;
+
